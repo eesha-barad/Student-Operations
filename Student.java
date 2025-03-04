bbd72ff9d5c4e236729300cfae9336f75a3f6bdd
eesha-barad <eeshabarad49@gmail.com>
2025-03-04 22:00:35 +0530

    added private attributes of student class

--- Student.java
+++ Student.java
@@ -1,1 +1,13 @@
- 
+ // Student.java
+public class Student {
+
+    // Private attributes of the Student class
+    private int prn;
+    private String name;
+    private String dob; 
+    private double marks;
+    private int position; 
+    // Constructor with only PRN
+    public Student(int prn) {
+        this.prn = prn;
+    }
